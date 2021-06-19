43b4e6e2f0fba3eda93f1fe498a46a07d2c9e7d0
CR7 <a.zauyod@fingerprint.ml>
2021-06-19 04:28:44 -0700

    v1

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -30,82 +30,85 @@ class _SerState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     var lan = Provider.of<LanguageProvider>(context, listen: true);
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          Text(
-            "${lan.getTexts('ads')}",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.03,
-          ),
-          Flexible(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AdsList(),
-              )),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
-          ),
-          Text(
-            "${lan.getTexts('service')}",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.03,
-          ),
-          Flexible(
-              flex: 8,
-              child: Container(
-                  color: Theme.of(context).canvasColor,
-                  child: FutureBuilder<List<Service>>(
-                    future: _futureAvailable,
-                    builder: (context, AsyncSnapshot snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else {
-                        if (snapshot.error != null) {
-                          return Text(snapshot.error.toString());
-                        }
-
-                        if (snapshot.hasData) {
-                          return ListView.builder(
-                            itemBuilder: (ctx, index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  Utils.NavigatorKey.currentState!
-                                      .pushNamed('/calendar');
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(8),
-                                  child: ServiceWidget(
-                                    title: snapshot.data![index].title,
-                                    color: snapshot.data![index].color,
-                                    subtitle: snapshot.data![index].subtitle,
-                                    svg: snapshot.data![index].svg,
-                                  ),
-                                ),
-                              );
-                            },
-                            itemCount: snapshot.data!.length,
+    return Directionality(
+      textDirection: lan.isEn ? TextDirection.ltr : TextDirection.rtl,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Text(
+              "${lan.getTexts('ads')}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Flexible(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AdsList(),
+                )),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Text(
+              "${lan.getTexts('service')}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Flexible(
+                flex: 8,
+                child: Container(
+                    color: Theme.of(context).canvasColor,
+                    child: FutureBuilder<List<Service>>(
+                      future: _futureAvailable,
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(),
                           );
                         } else {
-                          print('this data null ${snapshot.data}');
-                          return Center(child: Text('error'));
+                          if (snapshot.error != null) {
+                            return Text(snapshot.error.toString());
+                          }
+
+                          if (snapshot.hasData) {
+                            return ListView.builder(
+                              itemBuilder: (ctx, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Utils.NavigatorKey.currentState!
+                                        .pushNamed('/calendar');
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(8),
+                                    child: ServiceWidget(
+                                      title: snapshot.data![index].title,
+                                      color: snapshot.data![index].color,
+                                      subtitle: snapshot.data![index].subtitle,
+                                      svg: snapshot.data![index].svg,
+                                    ),
+                                  ),
+                                );
+                              },
+                              itemCount: snapshot.data!.length,
+                            );
+                          } else {
+                            print('this data null ${snapshot.data}');
+                            return Center(child: Text('error'));
+                          }
                         }
-                      }
-                    },
-                  )))
-        ],
+                      },
+                    )))
+          ],
+        ),
       ),
     );
   }
